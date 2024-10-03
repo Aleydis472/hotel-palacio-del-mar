f928a731066d7b7169f1a3b3db90e03d8cc0a0b7
Aleydis <alep472@gmail.com>
2024-10-03 15:09:12 -0500

    inicializo proyecto con algunas dependencias a usar.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  provideHttpClient(),
+  provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+  provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCbv3Crmp0DdWIUE5AyP97liHpyCbseQsk",
+        authDomain: "hotel-palacio-del-mar.firebaseapp.com",
+        projectId: "hotel-palacio-del-mar",
+        storageBucket: "hotel-palacio-del-mar.appspot.com",
+        messagingSenderId: "536651418301",
+        appId: "1:536651418301:web:b0f95a3ba55889161405d4"
+    }
+};
