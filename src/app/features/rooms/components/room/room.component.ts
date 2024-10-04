08599feb28a4806ae6a0d8cf7fe3c21a5e82189c
Aleydis <alep472@gmail.com>
2024-10-04 11:28:52 -0500

    Se importa componente de reservas.

--- src/app/features/rooms/components/room/room.component.ts
+++ src/app/features/rooms/components/room/room.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, Input, input } from '@angular/core';
 import { RoomService } from '../../../../services/room.service';
 import { Room } from '../../../../models/room.model';
 import { CommonModule } from '@angular/common';
+import { ReserveComponent } from '../reserve/reserve.component';
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReserveComponent],
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss'
 })
